4e9f15768377d067acacfb7081a0e706b6405106
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-10-18 14:12:59 -0400

    Quick Save

---
+++ src/main/java/com/ironyard/data/Kata.java
@@ -0,0 +1,7 @@
+package com.ironyard.data;
+
+/**
+ * Created by nathanielellsworth on 10/18/16.
+ */
+public class Kata {
+}
---
+++ src/main/java/com/ironyard/data/Main.java
@@ -0,0 +1,23 @@
+package com.ironyard.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nathanielellsworth on 10/18/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Loops();
+    }
+    //add ArrayList katas
+    //add HashMap katas
+
+    public static void Loops() {
+        List<Kata> myList = new ArrayList<Kata>();
+
+
+
+
+    }
+}
