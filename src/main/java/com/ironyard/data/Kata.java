99f9bc5bd3f0e7b8ae02ffb0f8f9ee6d045ddf9e
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-10-18 18:03:23 -0400

    Jesus

--- src/main/java/com/ironyard/data/Kata.java
+++ src/main/java/com/ironyard/data/Kata.java
@@ -1,7 +1,20 @@
 package com.ironyard.data;
 
+import java.util.List;
+
 /**
  * Created by nathanielellsworth on 10/18/16.
  */
-public class Kata {
+public class kata {
+    public static boolean firstLast6(List<Integer> x) {
+        x.get(0); // To get first
+        x.get(x.size()-1); //to get last
+        if(!(6==0 || 6==x.size()-1)){
+
+    return true;}
+        else {
+            return false;
+        }
+   }
+
 }
--- src/main/java/com/ironyard/data/Main.java
+++ src/main/java/com/ironyard/data/Main.java
@@ -7,17 +7,47 @@ import java.util.List;
  * Created by nathanielellsworth on 10/18/16.
  */
 public class Main {
+
     public static void main(String[] args) {
-        Loops();
+           firstlast6Example();
+        firstlast6Example2();
+
+
+        }
+   private static void firstlast6Example() {
+       List<Integer> myList = new ArrayList<Integer>();
+       myList.add(1);
+       myList.add(2);
+       myList.add(6);
+       boolean result = kata.firstLast6(myList);
+
+       System.out.printf("firstLast6(%s) -> %s\n", myList, result);
+
+   }
+
+    private static void firstlast6Example2() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(6);
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        boolean result = kata.firstLast6(myList);
+
+        System.out.printf("firstLast6(%s) -> %s\n", myList, result);
+
     }
-    //add ArrayList katas
-    //add HashMap katas
+    private static void firstlast6Example3() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(6);
+        myList.add(6);
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        boolean result = kata.firstLast6(myList);
 
-    public static void Loops() {
-        List<Kata> myList = new ArrayList<Kata>();
-
-
-
+        System.out.printf("firstLast6(%s) -> %s\n", myList, result);
 
     }
+
 }
+
