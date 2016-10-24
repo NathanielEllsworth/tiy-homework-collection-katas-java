fbde924b903215f3fbb7fc5d1f5e52b16bf08a29
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-10-23 22:00:05 -0400

    Almost done

--- src/main/java/com/ironyard/data/Kata.java
+++ src/main/java/com/ironyard/data/Kata.java
@@ -1,19 +1,155 @@
 package com.ironyard.data;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by nathanielellsworth on 10/18/16.
  */
+
+
 public class kata {
+
+
+
+
+
+
+    // (.1
     public static boolean firstLast6(List<Integer> x) {
 
         if(x.get(0) == 6 || x.get(x.size()-1) == 6){
             return true;
+        } else{
+            return false;
         }
-        else {return false;
+    }
+
+
+
+
+
+    // (.2
+    public static boolean sameFirstLast(List<Integer> x) {
+
+        if ((x.get(0) == x.get(x.size()-1) && x.get(0) == 1)){
+            return true;
+        }else{
+            return false;
         }
-   }
+    }
+
+
+
+
+
+    // (.3
+    public static void rotateLeft3(ArrayList<Integer> myList) {
+
+        int g = myList.remove(0);
+        myList.add(2,g);
+    }
+
+
+
+
+
+    // (.4
+    // method in class Main
+
+
+
+
+
+    // (.5
+    public static int sumFirstPenultimate(List<Integer>myList) {
+
+        int x = 0;
+        if (myList.size() >= 2) {
+            int firstElement = myList.get(0);
+            int secondToLast = myList.get(myList.size() - 2);
+            x = firstElement + secondToLast;
+        }else{
+            x =myList.get(0) + myList.get(1);
+        }
+        return x;
+    }
+
+
+
+
+
+    // (.6
+    public static void mapBully(HashMap<String,String> data){
+
+        String x = data.get("a");
+        data.put("b", x);
+        data.put("a", "\" \"");
+    }
+
+
+
+
+
+    // (.7
+    public static Map mapShare(HashMap<String,String> data) {
+
+        if (data.containsKey("a")) {
+            String x = data.get("a");
+            data.put("b", x);
+        }
+        data.remove("c");
+        return data;
+    }
+
+
+
+
+
+    // (.8
+    public static Map mapAB(HashMap<String,String> data){
+
+        if (data.containsKey("a") && data.containsKey("b")) {
+            String x = data.get("a") + data.get("b");
+            data.put("ab", x);
+        }
+        return data;
+    }
+
+
+
+
+
+    // (.9
+    public static Map<String, Integer> wordLen(ArrayList<String> strings) {
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : strings) {
+            if (map.get(str) == null){
+                map.put(str, str.length());
+            }
+        }
+        return map;
+    }
+
+
+
+
+
+
+    // (.10
+    public static HashMap<String, String> indexWords(String[] x) {
+
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < x.length; i++) {
+            String key = String.valueOf(x[i].charAt(0));
+            if (map.containsKey(key)) {
+                String val = map.get(key) + x[i];
+                map.put(key, val);
+            } else {
+                map.put(key, x[i]);
+            }
+        }return (HashMap<String, String>) map;
+    }
 
 }
 //collection.reverse
--- src/main/java/com/ironyard/data/Main.java
+++ src/main/java/com/ironyard/data/Main.java
@@ -1,7 +1,6 @@
 package com.ironyard.data;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by nathanielellsworth on 10/18/16.
@@ -9,35 +8,83 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        firstlast6Example();
-        firstlast6Example2();
-        firstlast6Example3();
+
+        // (.1
+        firstlast6v1();
+        firstlast6v2();
+        firstlast6v3();
+
+        // (.2
+        sameFirstLastv1();
+        sameFirstLastv2();
+        sameFirstLastv3();
+
+        // (.3
+        rotateLeft3v1();
+        rotateLeft3v2();
+        rotateLeft3v3();
+
+        // (.4
+        reverse3v1();
+        reverse3v2();
+        reverse3v3();
+
+        // (.5
+        sumFirstPenultimatev1();
+        sumFirstPenultimatev2();
+        sumFirstPenultimatev3();
+        sumFirstPenultimatev4();
+
+        // (.6
+        mapBullyv1();
+        mapBullyv2();
+        mapBullyv3();
+
+        // (.7
+        mapSharev1();
+        mapSharev2();
+        mapSharev3();
+
+        // (.8
+        mapABv1();
+        mapABv2();
+        mapABv3();
+
+        // (.9
+        wordLenv1();
+        wordLenv2();
+        wordLenv3();
+
+        // (.10
+        indexWordsv1();
+        indexWordsv2();
+        indexWordsv3();
+        indexWordsv4();
+    }
 
 
-        }
-   private static void firstlast6Example() {
-       List<Integer> myList = new ArrayList<Integer>();
-       myList.add(1);
-       myList.add(2);
-       myList.add(6);
-       boolean result = kata.firstLast6(myList);
-
-       System.out.printf("firstLast6(%s) -> %s\n", myList, result);
-
-   }
-
-    private static void firstlast6Example2() {
+    // (.1
+    private static void firstlast6v1() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(6);
+        boolean result = kata.firstLast6(myList);
+        System.out.println("");
+        System.out.printf("firstLast6(%s) -> %s\n", myList, result);
+        System.out.println("");
+    }
+    private static void firstlast6v2() {
         List<Integer> myList = new ArrayList<Integer>();
         myList.add(6);
         myList.add(1);
         myList.add(2);
         myList.add(3);
         boolean result = kata.firstLast6(myList);
-
         System.out.printf("firstLast6(%s) -> %s\n", myList, result);
-
+        System.out.println("");
     }
-    private static void firstlast6Example3() {
+    private static void firstlast6v3() {
         List<Integer> myList = new ArrayList<Integer>();
         myList.add(13);
         myList.add(6);
@@ -45,10 +92,334 @@ public class Main {
         myList.add(2);
         myList.add(3);
         boolean result = kata.firstLast6(myList);
-
         System.out.printf("firstLast6(%s) -> %s\n", myList, result);
-
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
+
+
+
+
+
+    // (.2
+    private static void sameFirstLastv1() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        boolean result = kata.sameFirstLast(myList);
+        System.out.printf("firstLast6(%s) -> %s\n", myList, result);
+        System.out.println("");
+    }
+    private static void sameFirstLastv2() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(1);
+        boolean result = kata.sameFirstLast(myList);
+        System.out.printf("firstLast6(%s) -> %s\n", myList, result);
+        System.out.println("");
+    }
+    private static void sameFirstLastv3() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(1);
+        boolean result = kata.sameFirstLast(myList);
+        System.out.printf("firstLast6(%s) -> %s\n", myList, result);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+
+
+
+
+    // (.3
+    private static void rotateLeft3v1() {
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        kata.rotateLeft3(myList);
+        System.out.println("sameFirstLast -> ("+myList+")");
+        System.out.println("");
+    }
+    private static void rotateLeft3v2() {
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(5);
+        myList.add(11);
+        myList.add(9);
+        kata.rotateLeft3(myList);
+        System.out.println("sameFirstLast -> ("+myList+")");
+        System.out.println("");
+    }
+    private static void rotateLeft3v3() {
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(7);
+        myList.add(0);
+        myList.add(0);
+        kata.rotateLeft3(myList);
+        System.out.println("sameFirstLast -> ("+myList+")");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+
+
+
+
+    // (.4
+    private static void reverse3v1() {
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        List<Integer> x = new ArrayList<Integer>(myList);
+        Collections.reverse(myList);
+        System.out.printf("reverse3(%s) -> %s\n", x, myList);
+        System.out.println("");
+    }
+    private static void reverse3v2() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(5);
+        myList.add(11);
+        myList.add(9);
+        List<Integer> x = new ArrayList<Integer>(myList);
+        Collections.reverse(myList);
+        System.out.printf("reverse3(%s) -> %s\n", x, myList);
+        System.out.println("");
+    }
+    private static void reverse3v3() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(7);
+        myList.add(0);
+        myList.add(0);
+        List<Integer> x = new ArrayList<Integer>(myList);
+        Collections.reverse(myList);
+        System.out.printf("reverse3(%s) -> %s\n", x, myList);
+        System.out.println("");
+        System.out.println("");
+    }
+
+
+
+
+
+    // (.5
+    private static void sumFirstPenultimatev1() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        List<Integer> x = new ArrayList<Integer>(myList);
+        int a = kata.sumFirstPenultimate(myList);
+        System.out.printf("sumFirstPenultimate(%s) -> %s\n", x, a);
+        System.out.println("");
+    }
+    private static void sumFirstPenultimatev2() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(1);
+        List<Integer> x = new ArrayList<Integer>(myList);
+        int a = kata.sumFirstPenultimate(myList);
+        System.out.printf("sumFirstPenultimate(%s) -> %s\n", x, a);
+        System.out.println("");
+    }
+    private static void sumFirstPenultimatev3() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(1);
+        myList.add(1);
+        myList.add(1);
+        List<Integer> x = new ArrayList<Integer>(myList);
+        int a = kata.sumFirstPenultimate(myList);
+        System.out.printf("sumFirstPenultimate(%s) -> %s\n", x, a);
+        System.out.println("");
+    }
+    private static void sumFirstPenultimatev4() {
+        List<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        List<Integer> x = new ArrayList<Integer>(myList);
+        int a = kata.sumFirstPenultimate(myList);
+        System.out.printf("sumFirstPenultimate(%s) -> %s\n", x, a);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+
+
+
+
+    // (.6      //having a little trouble here :/ all my hash functions are '=' not ':'
+    private static void mapBullyv1() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("a", "candy");
+        data.put("b", "dirt");
+        Object x = data.clone();
+        kata.mapBully(data);
+        System.out.printf("mapBully(%s) -> %s\n", x, data);
+        System.out.println("");
+    }
+    private static void mapBullyv2() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("b", "candy");
+        data.put("a", "candy");
+        Object x = data.clone();
+        kata.mapBully(data);
+        System.out.printf("mapBully(%s) -> %s\n", x, data);
+        System.out.println("");
+    }
+    private static void mapBullyv3() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("b", "carrot");
+        data.put("c", "meh");
+        data.put("a", "candy");
+        Object x = data.clone();
+        kata.mapBully(data);
+        System.out.printf("mapBully(%s) -> %s\n", x, data);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+
+
+
+
+    // (.7
+    private static void mapSharev1() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("b", "bbb");
+        data.put("c", "ccc");
+        data.put("a", "aaa");
+        Object x = data.clone();
+        kata.mapShare(data);
+        System.out.printf("mapShare(%s) -> %s\n", x, data);
+        System.out.println("");
+    }
+    private static void mapSharev2() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("b", "xyz");
+        data.put("c", "ccc");
+        Object x = data.clone();
+        kata.mapShare(data);
+        System.out.printf("mapShare(%s) -> %s\n", x, data);
+        System.out.println("");
+    }
+    private static void mapSharev3() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("d", "hi");
+        data.put("c", "meh");
+        data.put("a", "aaa");
+        Object x = data.clone();
+        kata.mapShare(data);
+        System.out.printf("mapShare(%s) -> %s\n", x, data);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+
+
+
+
+    // (.8
+    private static void mapABv1() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("b", "There");
+        data.put("a", "Hi");
+        Object x = data.clone();
+        kata.mapAB(data);
+        System.out.printf("mapAB(%s) -> %s\n", x, data);
+        System.out.println("");
+    }
+    private static void mapABv2() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("a", "Hi");
+        Object x = data.clone();
+        kata.mapAB(data);
+        System.out.printf("mapAB(%s) -> %s\n", x, data);
+        System.out.println("");
+    }
+    private static void mapABv3() {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("b", "There");
+        Object x = data.clone();
+        kata.mapAB(data);
+        System.out.printf("mapAB(%s) -> %s\n", x, data);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+
+        //VVVVVV   Need help with these last two    VVVVVVV
+
+
+    // (.9
+    private static void wordLenv1() {
+        ArrayList<String> data = new ArrayList<>();
+        data.add("this");
+        data.add("is");
+        data.add("hard");
+        data.add("ouch");
+        Object x = data.clone();
+        kata.wordLen(data);
+        System.out.printf("wordLen(%s) -> %s\n", x, data);
+    }
+
+
+
+
+    private static void wordLenv2() {
+        ArrayList<String> data = new ArrayList<>();
+        data.add("this");
+        data.add("that");
+        data.add("this");
+        data.add("that");
+        Object x = data.clone();
+        kata.wordLen(data);
+        System.out.printf("wordLen(%s) -> %s\n", x, data);
+    }
+
+
+
+
+    private static void wordLenv3() {
+        ArrayList<String> data = new ArrayList<>();
+        data.add("a");
+        data.add("dd");
+        data.add("a");
+        data.add("bb");
+        Object x = data.clone();
+        kata.wordLen(data);
+        System.out.printf("wordLen(%s) -> %s\n", x, data);
+    }
+
+
+
+
+
+
+
+    private static void indexWordsv1(){
+        String[] tmp = new String[]{"aardvark", "apple", "zamboni", "phone"};
+        String[] saved = tmp.clone();
+
+        Map<String, String> map =kata.indexWords(tmp);
+        System.out.println("IndexWords(\"" + Arrays.toString(saved) + "\") -> (\""+(map)+"\")");
+    }
+
+    
 
 }
 
